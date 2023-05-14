c884c806f2cb3189d0aeea8a79003608a692e23c
Benjamin <97377962+benjaminmwamba@users.noreply.github.com>
2023-05-14 00:48:50 -0500

    I removed board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,29 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/App.module.scss'
 import Board from '@/components/board/Board'
 import Keyboard from '@/components/keyboard/Keyboard'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function Home() {
+  const [board, setBoard] = useState<string[][]>([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ])
   
+
   return (
-    
     <div className={styles.app_container}>
       <nav className={styles.navbar}>
         <h2 className={styles.wordle_title}>wordle</h2>
       </nav>
       <section className={styles.lower_part}>
-        <Board />
+        <Board board={board} />
         <Keyboard />
       </section>
     </div>
