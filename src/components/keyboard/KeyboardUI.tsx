9265cb3b7645dfe1dd412a526c13c5379072eef9
Benjamin <97377962+benjaminmwamba@users.noreply.github.com>
2023-05-17 23:04:12 -0500

    now the keyboard layout uses objects

--- src/components/keyboard/KeyboardUI.tsx
+++ src/components/keyboard/KeyboardUI.tsx
@@ -4,12 +4,46 @@ import React, { useContext } from "react";
 import styles from "src/styles/Keyboard.module.scss"
 import Image from "next/image";
 import deleteIcon from "public/deleteIcon.svg"
+import { BRIGHT_GREY } from "@/utilities/colors";
 
 const keyboardKeys = [
-	["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
-	[EMPTY_STRING, "a", "s", "d", "f", "g", "h", "j", "k", "l", EMPTY_STRING],
-	["enter", "z", "x", "c", "v", "b", "n", "m", "fd"]
+	[{ color: EMPTY_STRING, text: "q" },
+	{ color: EMPTY_STRING, text: "w" },
+	{ color: EMPTY_STRING, text: "e" },
+	{ color: EMPTY_STRING, text: "r" },
+	{ color: EMPTY_STRING, text: "t" },
+	{ color: EMPTY_STRING, text: "y" },
+	{ color: EMPTY_STRING, text: "u" },
+	{ color: EMPTY_STRING, text: "i" },
+	{ color: EMPTY_STRING, text: "o" },
+	{ color: EMPTY_STRING, text: "p" }
+	],
+	[
+		{ color: EMPTY_STRING, text: EMPTY_STRING },
+		{ color: EMPTY_STRING, text: "a" },
+		{ color: EMPTY_STRING, text: "s" },
+		{ color: EMPTY_STRING, text: "d" },
+		{ color: EMPTY_STRING, text: "f" },
+		{ color: EMPTY_STRING, text: "g" },
+		{ color: EMPTY_STRING, text: "h" },
+		{ color: EMPTY_STRING, text: "j" },
+		{ color: EMPTY_STRING, text: "k" },
+		{ color: EMPTY_STRING, text: "l" },
+		{ color: EMPTY_STRING, text: EMPTY_STRING }
+	],
+	[
+		{ color: EMPTY_STRING, text: "enter" },
+		{ color: EMPTY_STRING, text: "z" },
+		{ color: EMPTY_STRING, text: "x" },
+		{ color: EMPTY_STRING, text: "c" },
+		{ color: EMPTY_STRING, text: "v" },
+		{ color: EMPTY_STRING, text: "b" },
+		{ color: EMPTY_STRING, text: "n" },
+		{ color: EMPTY_STRING, text: "m" },
+		{ color: EMPTY_STRING, text: "fd" }
+	]
 ];
+
 
 const KeyboardUI = () => {
 	return (
@@ -17,23 +51,20 @@ const KeyboardUI = () => {
 			<div className={styles.touchpad_container}>
 				{
 					keyboardKeys.map((keySlot, index) => {
-						const slotType = () => {
-							if (keySlot.includes("q")) return "first"
-							if (keySlot.includes(EMPTY_STRING)) return "second"
-							if (keySlot.includes("enter")) return "third"
-						}
 
 						return (
-							<div data-keyboard_slot_type={slotType()} key={(index + "ifdsj3")} className={styles.touchpad_keyslot}>
+							<div data-keyboard_slot_type key={(index + "ifdsj3")} className={styles.touchpad_keyslot}>
 								{
-									keySlot.map((key, index) => {
+									keySlot.map(({ color, text }, index) => {
 										return (
-											<div data-keyboard_key={key} key={(key + index)} className={styles.touchpad_key}>
+											<div style={{
+												backgroundColor: color === EMPTY_STRING ? BRIGHT_GREY : color
+											}} data-keyboard_key={text} key={(text + index)} className={styles.touchpad_key}>
 												{
-													key === "fd" ? <Image style={{
+													text === "fd" ? <Image style={{
 														color: "white",
 														fill: "white"
-													}} src={deleteIcon} alt="delete icon" /> : key
+													}} src={deleteIcon} alt="delete icon" /> : text
 												}
 											</div>
 										)
