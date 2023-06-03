f1430711ba89449b502ea5c5a534ecfea70075bf
Benjamin <97377962+benjaminmwamba@users.noreply.github.com>
2023-06-03 04:45:21 -0500

    fdffd

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{
+        height: "100dlv",
+        minHeight: "100dlv"
+      }}>
         <Main />
         <NextScript />
       </body>
