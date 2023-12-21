a851963d6b0e8327962ebc5b193318bbd9a99e34
Benjamin <97377962+benjaminmwamba@users.noreply.github.com>
2023-12-21 00:37:01 -0600

    I created the createAccount route

--- src/pages/Log_in.tsx
+++ src/pages/Log_in.tsx
@@ -13,6 +13,7 @@ const Login: React.FC = () => {
 		emailAddress: "",
 		password: '',
 	});
+	
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
 		const { name, value } = e.target;
@@ -30,18 +31,42 @@ const Login: React.FC = () => {
 
 	const [data, setData] = useState<any>(null)
 
+	const CREATE_ACCOUNT_ROUTE = '/api/createAccount';
+
+	
+
 	useEffect(() => {
+		const BENJAMIN_DUMMY_FORM_DATA = {
+			username: 'Benjamin Mwamba',
+			emailAddress: "benjaminmwamba75@gmail.com",
+			password: 'Benjamin2004',
+		}
 		const fetchData = async () => {
 			try {
-				const response = await fetch('/api/hello');
+				const lowerCaseUserData = {
+					username: BENJAMIN_DUMMY_FORM_DATA.username.toLowerCase(),
+					emailAddress: BENJAMIN_DUMMY_FORM_DATA.emailAddress.toLowerCase(),
+					password: BENJAMIN_DUMMY_FORM_DATA.password.toLowerCase(),
+				}
+				const response = await fetch(CREATE_ACCOUNT_ROUTE, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+						// You may need to include additional headers (e.g., authentication headers) here
+					},
+					body: JSON.stringify(lowerCaseUserData),
+				});
 				const result = await response.json();
 				setData(result);
-				console.log(result)
+				console.log(result.text)
+				console.table(result.users)
 			} catch (error) {
 				console.error('Error fetching data:', error);
 			}
-		};
 
+			
+		};
+		//if ((formData.username !== "") && (formData.emailAddress !== "") && (formData.password.length > 7))
 		fetchData();
 	}, []);
 
@@ -64,8 +89,8 @@ const Login: React.FC = () => {
 					<input
 						type="text"
 						id="email address"
-						name="email address"
-						value={formData.username}
+						name="emailAddress"
+						value={formData.emailAddress}
 						onChange={handleInputChange}
 						required
 					/>
--- src/pages/api/createAccount.ts
+++ src/pages/api/createAccount.ts
@@ -1,9 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import * as fs from 'fs';
-type Data = {
-	name: string
-}
 
 export interface User {
 	username: string,
@@ -15,27 +12,85 @@ export interface UserWithId extends User {
 	id: string
 }
 
-const filePath = 'path/to/your/file.json';
-const createAccount = (user: User): void => {
-	// Specify the path to your JSON file
+const USERS_JSON_FILE_PATH = 'src/pages/api/users.json';
+const getUsers = async (): Promise<UserWithId[]> => {
+	try {
+		// Read the JSON file asynchronously
+		const data = await fs.promises.readFile(USERS_JSON_FILE_PATH, 'utf8');
 
-	// Read the JSON file
-	fs.readFile(filePath, 'utf8', (err, data) => {
-		if (err) {
-			console.error('Error reading file:', err);
-			return;
-		}
+		// Parse the JSON data into a JavaScript array
+		const jsonArray: UserWithId[] = JSON.parse(data);
 
-		try {
-			// Parse the JSON data into a JavaScript object
-			const users: UserWithId[] = JSON.parse(data);
-
-			// Now, 'jsonArray' contains the array from the JSON file
-			console.log('Array from JSON file:', users);
-		} catch (parseError) {
-			console.error('Error parsing JSON:', parseError);
-		}
-	});
+		return jsonArray;
+	} catch (error) {
+		console.log(`Error reading or parsing JSON: ${error}`);
+		return []
+	}
 }
 
-export default createAccount
+const doesUserHaveAccount = async (user: User): Promise<boolean> => {
+	try {
+		const users = await getUsers();
+
+		const doesUserExistInUsers = users.some((singleUser) =>
+			singleUser.username === user.username && singleUser.emailAddress === user.emailAddress
+		);
+
+		return doesUserExistInUsers;
+	} catch (error) {
+		console.error('Error fetching users:', error);
+		// Handle the error accordingly, e.g., return false or throw an exception
+		return false;
+	}
+};
+const addUserToUsersList = async (newUser: User): Promise<boolean> => {
+	try {
+		// Get the existing users
+		const users = await getUsers();
+
+		const newUserWithId = {
+			...newUser,
+			id: newUser.username + newUser.password,
+		}
+		// Add the new user to the array
+		users.push(newUserWithId);
+
+		// Write the updated array back to the file
+		//fs.writeFile(USERS_JSON_FILE_PATH, JSON.stringify(users, null, 2), 'utf8');
+		await fs.writeFile(USERS_JSON_FILE_PATH, JSON.stringify(users, null, 2), (err) => console.log(err));
+
+		console.log('User added successfully');
+		return true
+	} catch (error) {
+		console.error('Error adding user:', error);
+		return false
+	}
+}
+
+type Data = any;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+	res.status(200);
+	try {
+		const newUser = req.body;
+		const doesUserExist = await doesUserHaveAccount(newUser);
+		if (doesUserExist === false) {
+			const wasUserAdded = addUserToUsersList(newUser)
+			if (await wasUserAdded === true) {
+				res.status(200)
+
+				res.send({
+					text: "user was created.",
+					users: await getUsers()
+				})
+			} else {
+				res.send({text: "user not created"})
+			}
+		} else {
+			res.send({text: "user already exists"})
+		}
+	} catch (error) {
+		res.send({ text: "there was an error" })
+	}
+}
+
